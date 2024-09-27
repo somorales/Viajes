d16005f7c601c1f5deb76dbcf0ac73be54d36cb2
Sofia Morales <sofimoralesroa92@gmail.com>
2024-09-27 19:16:06 +0200

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import { useState } from 'react'
+import './App.css'
 import{ Routes, Route } from 'react-router-dom'
-import 
 
 function App() {
-  return (
-<Routes>
 
-</Routes>
+
+  return(
+
+  <>
+  <Navbar/>
+    
+  <Routes>
+
+  <Route path="/" element={<HomePage />} />
+  <Route path="/:city" element={<CityRecomendation />} />
+  <Route path="/:recomendationId" element={<RecomendationDetails />} />
+  <Route path="/create" element={<CreateRecomendation />} />
+  <Route path="/about" element={<About />} />
+  <Route path="*" element={<Error />} />
+  <Route path="/edit" element={<EditRecomendation />} />
+  <Route path="preview" element={<CardPreview />} />
+
+  </Routes>
+  
+  <Footer />
+   </>
+   
 
   )
-
 }
+  
+
 
 export default App
