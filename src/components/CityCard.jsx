12d5c68c99494fd1d749c683492aa9da5e089ced
Angela Ruiz <angela.ruiz.rodriguez.8@gmail.com>
2024-10-01 22:32:53 +0200

    pequeñas correcciones

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { useParams } from 'react-router'
 
 function CityCard(props) {
 
   const {eachCity} = props
 
   return (
-    <div className='city-card' key={eachCity.id}  style={{backgroundImage:`url (${eachCity.image})`}}>
-      <img src={eachCity.image} alt={eachCity.city} />
+    <div className='city-card' key={eachCity.id}  style={{backgroundImage:`url(${eachCity.image})`}}>
       <h2>{eachCity.city}</h2>
     </div>
   )
