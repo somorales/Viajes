c9f4acaf9adb718e1b3d6503a29ad208c42aa93a
Angela Ruiz <angela.ruiz.rodriguez.8@gmail.com>
2024-10-02 17:20:52 +0200

    estilos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,29 +3,24 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-
-    <div>
+<>
     <div className="containerNavbar">
-      <div className="contentNavbar">
-        <div className="containerLogoNavbar">
-          <h1>TravelStories</h1>
+      <nav className="nav-bar">
+        <div className="nav-bar-logo">
+        <Link to={"/"}><h1>TravelStories</h1></Link>
         </div>
-        <div className="containerLink">
+        <ul className="nav-bar-links">
           <Link to={"/"} style={{ textDecoration: "none" }}>
-            <h5>Home</h5>
+            <li>Home</li>
           </Link>
           <Link to={"/about"} style={{ textDecoration: "none" }}>
-            <h5>About</h5>
+            <li>About</li>
           </Link>
-       
-        <Link to={"/create"} style={{ textDecoration: "none" }}> 
-            <h5>Create TravelStories</h5>
-        </Link>
-        </div>
-    </div>
+        </ul>
+    </nav>
     <div className="navbar-linea"></div>
     </div>
-  </div>
+    </>
 );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import CityCard from '../components/CityCard'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import CityCard from "../components/CityCard";
+import { Link } from "react-router-dom";
 
 function HomePage(props) {
-    
-  const {cities}=props
+  const { cities } = props;
 
-  console.log(cities)
-
+  console.log(cities);
 
   return (
     <>
-     <h1 className='main-title'>Choose a city</h1>
-    <div id='cities-box'>
-      {cities.map((eachCity)=>{
-
-        return (
-          <Link to={`/${eachCity.city}`}> <CityCard key={eachCity.id} eachCity={eachCity}/></Link>
-        )
-      })}
-    </div>
+      <div id="home-header">
+        <p>Tell us your story, create new ones.</p>
+      </div>
+      <div>
+        <h1 className="main-title">Choose a city</h1>
+        <div id="cities-box">
+          {cities.map((eachCity) => {
+            return (
+              <Link to={`/${eachCity.city}`}>
+                {" "}
+                <CityCard key={eachCity.id} eachCity={eachCity} />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
